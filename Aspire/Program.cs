1239355bfeeedfb7378d685afa5133e235fc0273
Jérôme Piquot <jpiquot@itaneo.com>
2024-11-26 17:23:03 +0100

    Update Dapr configs and project settings
    Updated `statestore.yaml` and `config.yaml` to change Dapr component and configuration settings, including Redis state store and tracing/metrics. Modified `Program.cs` to rename projects and update environment configurations. Updated `appsettings.json` to revise various settings, removing old ones and adding new ones, including changes to the `Security` section and addition of a `Document` section.

--- Aspire/Program.cs
+++ Aspire/Program.cs
@@ -26,8 +26,7 @@ app.Builder.Configuration.AddUserSecrets<Program>();
 if (app.IsProjectEnabled<Projects.HexalithApp_WebServer>())
 {
     _ = app
-        .AddProject<Projects.HexalithApp_WebServer>("security-web")
-        .WithEnvironmentFromConfiguration("Hexalith__Security__UseMsal")
+        .AddProject<Projects.HexalithApp_WebServer>("securityweb")
         .WithEnvironmentFromConfiguration("Hexalith__Security__Enabled")
         .WithEnvironmentFromConfiguration("EmailServer__ApplicationSecret")
         .WithEnvironmentFromConfiguration("EmailServer__FromEmail")
@@ -38,10 +37,18 @@ if (app.IsProjectEnabled<Projects.HexalithApp_WebServer>())
         .WithEnvironmentFromConfiguration("AzureAd__ClientId");
 }
 
-if (app.IsProjectEnabled<Projects.HexalithApp_WebServer>())
+if (app.IsProjectEnabled<Projects.HexalithApp_ApiServer>())
 {
     _ = app
-        .AddProject<Projects.HexalithApp_ApiServer>("security-api");
+        .AddProject<Projects.HexalithApp_ApiServer>("securityapi")
+        .WithEnvironmentFromConfiguration("Hexalith__Security__Enabled")
+        .WithEnvironmentFromConfiguration("EmailServer__ApplicationSecret")
+        .WithEnvironmentFromConfiguration("EmailServer__FromEmail")
+        .WithEnvironmentFromConfiguration("EmailServer__FromName")
+        .WithEnvironmentFromConfiguration("AzureAd__Instance")
+        .WithEnvironmentFromConfiguration("AzureAd__Domain")
+        .WithEnvironmentFromConfiguration("AzureAd__TenantId")
+        .WithEnvironmentFromConfiguration("AzureAd__ClientId");
 }
 
 await app
